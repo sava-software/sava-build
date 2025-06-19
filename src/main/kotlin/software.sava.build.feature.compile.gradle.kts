e982c30e9feb4975594b861b63fd20568998fb45
jpe7s <jpe7s.salt188@passfwd.com>
2025-06-19 09:13:51 -0500

    Default all builds to Java 24.

--- src/main/kotlin/software.sava.build.feature.compile.gradle.kts
+++ src/main/kotlin/software.sava.build.feature.compile.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 @Suppress("UnstableApiUsage")
 val javaVersion = providers.fileContents(isolated.rootProject.projectDirectory.file("gradle/java-version.txt")).asText
 
-val jlv = JavaLanguageVersion.of(providers.gradleProperty("javaVersion").orElse(javaVersion).get().trim())
+val jlv = JavaLanguageVersion.of(providers.gradleProperty("javaVersion").orElse(javaVersion).getOrElse("24").trim())
 
 java {
   toolchain.languageVersion = jlv
