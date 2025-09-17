286bc7527b7a4bb8c52105cbb1127086026b891a
jpe7s <jpe7s.salt188@passfwd.com>
2025-09-17 10:26:08 -0500

    Disable dependency-analysis plugin until they add support for Java 25.

--- src/main/kotlin/software.sava.build.check.dependencies.gradle.kts
+++ src/main/kotlin/software.sava.build.check.dependencies.gradle.kts
@@ -1,13 +1,13 @@
-import org.gradlex.javamodule.dependencies.tasks.ModuleDirectivesOrderingCheck
-import org.gradlex.javamodule.dependencies.tasks.ModuleDirectivesScopeCheck
-
-plugins {
-  id("java")
-  id("com.autonomousapps.dependency-analysis")
-}
-
-tasks.withType<ModuleDirectivesOrderingCheck>().configureEach { enabled = false }
-
-tasks.check {
-  dependsOn(tasks.withType<ModuleDirectivesScopeCheck>())
-}
+//import org.gradlex.javamodule.dependencies.tasks.ModuleDirectivesOrderingCheck
+//import org.gradlex.javamodule.dependencies.tasks.ModuleDirectivesScopeCheck
+//
+//plugins {
+//  id("java")
+//  id("com.autonomousapps.dependency-analysis")
+//}
+//
+//tasks.withType<ModuleDirectivesOrderingCheck>().configureEach { enabled = false }
+//
+//tasks.check {
+//  dependsOn(tasks.withType<ModuleDirectivesScopeCheck>())
+//}
