bf14eba6b12a95cca032aa4affb16b2026294cdc
jpe7s <jpe7s.salt188@passfwd.com>
2025-12-10 19:52:35 -0600

    Default to ORACLE vendor which includes modules.

--- src/main/kotlin/software.sava.build.feature.compile.gradle.kts
+++ src/main/kotlin/software.sava.build.feature.compile.gradle.kts
@@ -6,6 +6,5 @@ val languageVersion = JavaLanguageVersion.of(javaVersion("25"))!!
 
 java {
   toolchain.languageVersion = languageVersion
-  @Suppress("UnstableApiUsage")
-  toolchain.vendor = JvmVendorSpec.JETBRAINS
+  toolchain.vendor = JvmVendorSpec.ORACLE
 }
